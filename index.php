915ce7f7e12dfa222b518a724d9046c6d148504a
dhalecod <williammakinde408@gmail.com>
2024-11-25 17:33:45 +0000

    updated

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 //here is a detailed info above Dev Marcus
 $devMarcusinfo = "coding, gyming, studyng, and singing";
-echo "Dev_Marcus is a software engineer and his favorite activities are" . $devMarcusinfo;
+echo "Dev_Marcus is a software engineer and his favorite activities are " . $devMarcusinfo;
 ?>
