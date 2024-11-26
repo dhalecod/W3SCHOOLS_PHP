94308c15f3f5c5b6f059cf01928191da5ce082ca
dhalecod <williammakinde408@gmail.com>
2024-11-26 14:26:19 +0000

    updated

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$devMarcusinfo = "coding, gyming, studyng, and singing";
-print 'I am Dev_Marcus my best hobbies are ' . $devMarcusinfo;
+$myFutureDreamCarsModel = array("MERCEDEZ BENZ", "RANGE ROVER", "BMW");
+var_dump($myFutureDreamCarsModel);
 ?>
