c8fbd3f3826c1cbdc8098323cbe6326e78792b9f
dhalecod <williammakinde408@gmail.com>
2024-11-26 10:40:45 +0000

    updated

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $devMarcusinfo = "coding, gyming, studyng, and singing";
-echo 'I am Dev_Marcus, My hobbies are' . $devMarcusinfo;
+echo 'I am Dev_Marcus, My hobbies are ' . $devMarcusinfo;
 ?>
