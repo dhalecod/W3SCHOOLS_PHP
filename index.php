24ad856a9322331cc3d77a4b83b8cb976c28bb2c
dhalecod <williammakinde408@gmail.com>
2024-11-26 08:33:31 +0000

    updated

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $devMarcusinfo = "coding, gyming, studyng, and singing";
-echo "<h1>I am Dev_Marcus, My hobbies are $devMarcusinfo</h1>";
+echo "I am Dev_Marcus, My hobbies are $devMarcusinfo";
 ?>
